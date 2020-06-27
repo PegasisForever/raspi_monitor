47e5a043fbdae3e6f0ffef897940ec525d961e1b
pegasis <admin@pegasis.site>
2020-06-26 22:55:10 -0400

    added network, change f32 to f64

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn read_file(name: &str) -> Result<String, Error> {
     Ok(content)
 }
 
-fn add_json_f32(json: &mut Json, key: &str, value: f32) {
+fn add_json_f64(json: &mut Json, key: &str, value: f64) {
     json.add(
         Json::OBJECT {
             name: String::from(key),
@@ -44,42 +44,59 @@ fn main() {
     } else {
         let sys = System::new();
         let mut json = Json::new();
-        add_json_f32(&mut json, "v", 1.0);
+        add_json_f64(&mut json, "v", 1.0);
 
         if let Ok(temp) = sys.cpu_temp() {
-            add_json_f32(&mut json, "cpu_temp", temp);
+            add_json_f64(&mut json, "cpu_temp", temp as f64);
         }
         if let Ok(mem) = sys.memory() {
-            let mem_used = (saturating_sub_bytes(mem.total, mem.free).as_u64() as f32) / 1024.0 / 1024.0;
-            let mem_total = (mem.total.as_u64() as f32) / 1024.0 / 1024.0;
-            add_json_f32(&mut json, "mem_used_mb", mem_used);
-            add_json_f32(&mut json, "mem_total_mb", mem_total);
+            let mem_used = (saturating_sub_bytes(mem.total, mem.free).as_u64() as f64) / 1024.0 / 1024.0;
+            let mem_total = (mem.total.as_u64() as f64) / 1024.0 / 1024.0;
+            add_json_f64(&mut json, "mem_used_mb", mem_used);
+            add_json_f64(&mut json, "mem_total_mb", mem_total);
         }
         if let Ok(load) = sys.load_average() {
-            add_json_f32(&mut json, "load_1", load.one);
-            add_json_f32(&mut json, "load_5", load.five);
-            add_json_f32(&mut json, "load_15", load.fifteen);
+            add_json_f64(&mut json, "load_1", load.one as f64);
+            add_json_f64(&mut json, "load_5", load.five as f64);
+            add_json_f64(&mut json, "load_15", load.fifteen as f64);
         }
         {
             let stdout = read_file("/proc/stat").unwrap();
             let first_line: &str = stdout.split("\n").next().unwrap();
-            let cpu_times: Vec<f32> = first_line.split(" ")
-                .skip(2)
-                .map(|num| num.parse::<f32>().unwrap())
+            let cpu_times: Vec<f64> = first_line.split_whitespace()
+                .skip(1)
+                .map(|num| num.parse::<f64>().unwrap())
                 .collect();
             let idle_time = cpu_times[3];
-            let total_time = cpu_times.iter().sum::<f32>();
-            add_json_f32(&mut json, "cpu_idle_time", idle_time);
-            add_json_f32(&mut json, "cpu_total_time", total_time);
+            let total_time = cpu_times.iter().sum::<f64>();
+            add_json_f64(&mut json, "cpu_idle_time", idle_time);
+            add_json_f64(&mut json, "cpu_total_time", total_time);
         }
         if let Ok(cpu_hertz) = read_file_as_float("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq") {
-            add_json_f32(&mut json, "cpu_mhz", cpu_hertz / 1024.0);
+            add_json_f64(&mut json, "cpu_mhz", (cpu_hertz as f64) / 1024.0);
         }
         if let Ok(cpu_min_hertz) = read_file_as_float("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq") {
-            add_json_f32(&mut json, "cpu_min_mhz", cpu_min_hertz / 1024.0);
+            add_json_f64(&mut json, "cpu_min_mhz", (cpu_min_hertz as f64) / 1024.0);
         }
         if let Ok(cpu_max_hertz) = read_file_as_float("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq") {
-            add_json_f32(&mut json, "cpu_max_mhz", cpu_max_hertz / 1024.0);
+            add_json_f64(&mut json, "cpu_max_mhz", (cpu_max_hertz as f64) / 1024.0);
+        }
+        {
+            let stdout = read_file("/proc/net/dev").unwrap();
+            println!("{}", stdout);
+            let mut total_received_bytes = 0.0;
+            let mut total_sent_bytes = 0.0;
+            stdout.split("\n")
+                .skip(2)
+                .for_each(|line: &str| {
+                    let nums = line.split_whitespace().collect::<Vec<&str>>();
+                    if nums.is_empty() || nums[0] == "lo:" { return; }
+                    let mid = nums.len() / 2;
+                    total_received_bytes += nums[1].parse::<f64>().unwrap();
+                    total_sent_bytes += nums[mid + 1].parse::<f64>().unwrap();
+                });
+            add_json_f64(&mut json, "received_bytes", total_received_bytes);
+            add_json_f64(&mut json, "sent_bytes", total_sent_bytes);
         }
 
         println!("{}", json.print());
